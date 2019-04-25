5c6fefc5623f96ea5166f91d856b47181a34321a
Anna Xu <annaxu@annaxus-mbp.uphs.upenn.edu>
2019-04-25 09:19:41 -0400

    ALE scripts and dependencies

---
+++ painHealthyALE.m
@@ -0,0 +1,22 @@
+%% Compute ALE for PainHealthy Project
+% by AX 04/01/2019
+% -------------------------------------
+% This script is a higher-level script that runs all operations pertaining to
+% computing ALE (using Eickhoff's scripts)
+%   a) thresholds the main effects clusters at z>3.09
+%   b) generates the negative & positive contrast images for contrasts
+%   c) thresholds the contrast clusters at a liberal threshold of z>1.6 and k>50
+%       % N.B. : this will eventually get re-thresholded with FSL at z>3.09
+%   d) thresholds the conjunction clusters at z>3.09 and k>15
+% -------------------------------------
+
+%% Setup
+
+setupPath(pwd);
+cd EickhoffALE
+
+%% Script
+    % input coordinates
+ale_inputCoords('data/painHealthyCoords_20190401.xls');
+    % run analyses
+ale_estimateALE('contrasts/painHealthyAnalyses_20190401.xlsx');
