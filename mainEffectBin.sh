5281fddd990481d792f290c364152d0772fd621c
Anna Xu <annaxu@annaxus-mbp.uphs.upenn.edu>
2019-09-18 13:40:24 -0400

    include pain > rest in overlap figure

--- mainEffectBin.sh
+++ mainEffectBin.sh
@@ -5,8 +5,8 @@ for main in $niftiMainEffect; do
     mainEffectName=$(echo $main|cut -d '/' -f4|cut -d '_' -f1);
 #   Binarize each main effects map
 #   Outputs: nifti binarized (1 if convergent activation was present)
-    fslmaths ${main} -bin ${mainEffectName}_bin 
+    fslmaths ${main} -bin ${mainEffectName}_bin
 done
 
 # add the figures on top of each other to view overlap
-fslmaths activation_bin.nii.gz -add armLeg_bin.nii.gz -add chemical_bin.nii.gz -add electrical_bin.nii.gz -add female_bin.nii.gz -add handFoot_bin.nii.gz -add left_bin.nii.gz -add male_bin.nii.gz -add mechanical_bin.nii.gz -add notVisceral_bin.nii.gz -add otherMod_bin.nii.gz -add painInnoc_bin.nii.gz -add right_bin.nii.gz -add thermal_bin.nii.gz -add visceral_bin.nii.gz overlap_map_bin.nii.gz
+fslmaths activation_bin.nii.gz -add armLeg_bin.nii.gz -add chemical_bin.nii.gz -add electrical_bin.nii.gz -add female_bin.nii.gz -add handFoot_bin.nii.gz -add left_bin.nii.gz -add male_bin.nii.gz -add mechanical_bin.nii.gz -add notVisceral_bin.nii.gz -add otherMod_bin.nii.gz -add painInnoc_bin.nii.gz -add painBaseline_bin.nii.gz -add right_bin.nii.gz -add thermal_bin.nii.gz -add visceral_bin.nii.gz overlap_map_bin.nii.gz
